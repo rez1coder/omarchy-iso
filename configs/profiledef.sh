5756f35844a576ab05a73c9c7ac5575a1a5be783
Ryan Hughes <ryan@heyoodle.com>
2026-02-19 12:44:54 -0500

    Update pacman conf to use for build

--- configs/profiledef.sh
+++ configs/profiledef.sh
@@ -10,7 +10,7 @@ install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux' 'uefi.grub')
 arch="x86_64"
-pacman_conf="pacman.conf"
+pacman_conf="pacman-offline.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
